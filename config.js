7183b148af7dcc8e8333462eded10c23d8d172ac
ccareyncsu <ccarey@ncsu.edu>
2018-07-12 00:00:12 -0600

    added mlabdb

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://dbuser:dbpassword1@ds133621.mlab.com:33621/node-restaurants-app-mongoose',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://dbuser:dbpassword1@ds133601.mlab.com:33601/chasen-noteful-v3',
   TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test'
 };
