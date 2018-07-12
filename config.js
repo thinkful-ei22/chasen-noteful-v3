678847e0a6ec14855c1f1199285328fa99094996
ccareyncsu <ccarey@ncsu.edu>
2018-07-12 12:02:46 -0600

    changed test server

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb://dbuser:dbpassword1@ds133601.mlab.com:33601/chasen-noteful-v3',
-  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost:27017/noteful-test'
+  TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test'
 };
