2bab6cb7264391e37bae9a7494696925ba1f6eb6
ccareyncsu <ccarey@ncsu.edu>
2018-07-12 16:21:46 -0600

    seeded database with utils

--- routes/notes.js
+++ routes/notes.js
@@ -30,16 +30,16 @@ router.get('/', (req, res, next) => {
 });
 
 /* ========== GET/READ A SINGLE ITEM ========== */
-router.get('/:_id', (req, res, next) => {
-  const { _id } = req.params;
-
-  if (!mongoose.Types.ObjectId.isValid(_id)) {
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     const err = new Error('The `id` is not valid');
     err.status = 400;
     return next(err);
   }
 
-  Note.findById(_id)
+  Note.findById(id)
     .then(result => {
       if (result) {
         res.json(result);
